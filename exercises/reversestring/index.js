328ff3c0cb4c847afec9164f911c1dbe943b23c5
Danielle Kogan <kogandanielle@gmail.com>
2020-05-08 17:40:08 -0400

    reverse sol1

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,16 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  //the bruteforce method would be to start at the end of the string and loop through
+  //appending each item to an output String
+  //The bigO would be Linear; the length of the string. The Space complexity would be Constant
+  let outputStr = '';
+  for(let i = str.length - 1; i >= 0; i--) {
+    outputStr += str[i];
+  }
+  console.log(outputStr);
+  return outputStr;
+}
 
 module.exports = reverse;
