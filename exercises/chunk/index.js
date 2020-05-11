6ea6715be080629ade6a181e8590e365f86dd43e
Danielle Kogan <kogandanielle@gmail.com>
2020-05-11 17:03:33 -0400

    a few more

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,39 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  //we can reduce the space complexity if we don't store in named variables
+  let outputArr = [];
+  for (let i = 0; i < array.length; i++) {
+    if(!outputArr[outputArr.length - 1] || outputArr[outputArr.length - 1].length >= size) {
+      outputArr.push([array[i]])
+    } else {
+      outputArr[outputArr.length - 1].push(array[i])
+    }
+  }
+  return outputArr;
+}
 
 module.exports = chunk;
+
+  //the function takes in an array and returns an array of arrays w/ length of size
+  //brute force would be to loop through the array and create subarrays
+  // let outputArr = [];
+  // let sizeCounter = 1;
+  // let subArray = [];
+  // for (let i = 0; i < array.length; i++) {
+  //   if(sizeCounter <= size) {
+  //     subArray.push(array[i])
+  //     sizeCounter++;
+  //   } else {
+  //     outputArr.push(subArray)
+  //     sizeCounter = 2;
+  //     subArray = [];
+  //     subArray.push(array[i])
+  //   }
+  // }
+  // if (subArray.length > 0) {
+  //   outputArr.push(subArray);
+  // }
+  // console.log(outputArr);
+  // return outputArr;
--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,18 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  //for i= 1 until i = n log num
+  //if num modulus 3 + modulus 5 is 0 'fizzbuzz'
+  //else if modulus 3 fizz
+  //else if modulus 5 buzz
+  //else print num
+  for (let i = 1; i <= n; i++) {
+    if((i%3 + i%5) === 0) console.log('fizzbuzz');
+    else if(i%3 === 0) console.log('fizz');
+    else if (i%5 === 0) console.log('buzz');
+    else console.log(i);
+  }
+}
 
 module.exports = fizzBuzz;
