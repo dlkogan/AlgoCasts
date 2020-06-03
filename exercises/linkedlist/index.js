379cf93d7168a07fcfdd7892a2876b47926996e3
Danielle Kogan <kogandanielle@gmail.com>
2020-06-03 19:01:44 -0400

    linkedList work

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,98 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(val, next = null) {
+    this.data = val,
+    this.next = next
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null
+  }
+  insertFirst(val) {
+    let node = new Node(val);
+    if(!this.head) {
+      this.head = node;
+    } else {
+      const oldHead = this.head;
+      node.next = oldHead;
+      this.head = node;
+    }
+  }
+  size() {
+    let size = 0;
+    if(!this.head) return 0;
+    let currNode = this.head;
+    while(currNode) {
+      size++;
+      currNode = currNode.next;
+    }
+    return size;
+  }
+  getFirst() {
+    return this.head;
+  }
+  getLast() {
+    let currNode = this.head;
+    while(currNode.next) {
+      currNode = currNode.next;
+    }
+    return currNode;
+  }
+  clear() {
+    this.head = null;
+  }
+  removeFirst() {
+    if(!this.head) return;
+    const newHead = this.head.next;
+    this.head = newHead;
+  }
+  removeLast() {
+    if(!this.head) return;
+    if(this.size() === 1) {
+      this.head = null;
+      return;
+    }
+    let currNode = this.head;
+    let lastNode = currNode;
+    while(currNode.next) {
+      lastNode = currNode;
+      currNode = currNode.next;
+    }
+    lastNode.next = null;
+  }
+  insertLast(val) {
+    const newNode = new Node(val);
+    if(!this.head) {
+      this.head = newNode;
+      return;
+    }
+    let currNode = this.head
+    while(currNode.next) {
+      currNode = currNode.next;
+    }
+    currNode.next = newNode;
+  }
+  getAt(val) {
+    let currLocation = 0;
+    let currNode = this.head;
+    if(!currNode) return null;
+    while(val !== currLocation && currNode) {
+      currLocation++
+      currNode = currNode.next;
+    }
+    if(currLocation === val) {
+      return currNode
+    } else {
+      return null;
+    }
+  }
+  removeAt(ind) {
+
+  }
+}
 
 module.exports = { Node, LinkedList };
