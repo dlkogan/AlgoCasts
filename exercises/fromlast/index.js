d03feadadd403939d31f543bbbab9a4b644accd1
Danielle Kogan <kogandanielle@gmail.com>
2020-06-05 14:37:05 -0400

    linkedList section

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,6 +11,30 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+  //we have our list
+  //we want a fast and slow pointer both starting at the beginning
+  //we will set slow to start and fast to n from the start
+  //then advance by 1 each
+  //we know that fast is always n away from slow
+  //so when fast reaches the n, slow is where it needs to be
+
+  /*
+  1
+  1 - 2 - 3 - 4
+          s
+
+  */
+  let slow = list.head;
+  let fast = list.head;
+  for(let i = 0; i <= n; i++) {
+    fast = fast.next;
+  }
+  while(fast) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+  return slow;
+}
 
 module.exports = fromLast;
